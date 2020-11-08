9170235e35091f27d5e939017e4d4132d2ee6ea4
Thomas Tastet <31560900+ThomasT404@users.noreply.github.com>
2020-11-08 15:51:36 +0100

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,8 +1,6 @@
 export { ServerRequest } from "https://deno.land/std@0.61.0/http/server.ts";
 
-export {
-  MultipartReader,
-  FormFile,
-} from "https://deno.land/std@0.61.0/mime/multipart.ts";
+export { MultipartReader } from "https://deno.land/std@0.61.0/mime/multipart.ts";
+export type { FormFile } from "https://deno.land/std@0.61.0/mime/multipart.ts";
 
 export * as bytes from "https://deno.land/std@0.61.0/bytes/mod.ts";
