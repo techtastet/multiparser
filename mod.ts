559e30b71c9e3438cc986daffeb60bdce44e6133
Thomas Tastet <31560900+ThomasT404@users.noreply.github.com>
2020-11-08 13:28:42 +0100

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,9 +1,10 @@
 export { multiParser } from "./lib/multiParser.ts";
 
-export {
+export { multiParser as multiParserV2 } from "./lib/multiParserV2.ts";
+
+export type {
   Form as FormV2,
   FormFile as FormFileV2,
-  multiParser as multiParserV2,
 } from "./lib/multiParserV2.ts";
 
-export { FormFile } from "https://deno.land/std@0.61.0/mime/multipart.ts";
+export type { FormFile } from "https://deno.land/std@0.61.0/mime/multipart.ts";
